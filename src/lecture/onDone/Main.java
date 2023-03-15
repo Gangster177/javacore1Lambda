b29669ef48a66b378f0ba21ce86e70c96258cbad
Nikita Novikov <monstars51@gmail.com>
2023-03-15 15:30:39 +0300

    add package onDone v2

--- src/lecture/onDone/Main.java
+++ src/lecture/onDone/Main.java
@@ -1,12 +1,7 @@
 package lecture.onDone;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<String> cities = Arrays.asList("Мадрид", "Париж", "Москва", "Токио");
-
         OnDoneListener listener = (v) -> System.out.println(v);
         Test test = new Test(listener);
         test.execute();
